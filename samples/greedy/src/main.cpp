5ebe8311514334cb2571b6cb4f04301842302167
Andrew Chmutov <achmutov@proton.me>
2025-10-12 07:47:01 +0200

    feat: display csv format
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- samples/greedy/src/main.cpp
+++ samples/greedy/src/main.cpp
@@ -3,6 +3,8 @@
 #include <evolutionary_computation/loader/csv.h>
 #include <evolutionary_computation/solver/random.h>
 #include <stdexcept>
+#include <ranges>
+#include <limits.h>
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
@@ -18,18 +20,39 @@ int main(int argc, char* argv[]) {
         std::exit(64);
     }
 
+    std::cout << "instance,method,min,max,mean,best_solution\n";
+
     for (int i = 2; i < argc; i++) {
-        auto stream = std::ifstream(argv[i]);
+        auto const instancePath = argv[i];
+        auto stream = std::ifstream(instancePath);
         auto loader = CSVLoader(stream);
         auto data = loader.load();
         auto solver = RandomSolver(data);
+        float minObj = INT_MAX;
+        float maxObj = -1;
+        float meanObj = 0;
+        Solver::Indices best;
         for (int j = 0; j < n; j++) {
-            auto [indices, result] = solver.solve();
-            for (auto i : indices) {
-                std::cout << i << ' ';
+            auto const [indices, candidate] = solver.solve();
+            auto const fcandidate = static_cast<float>(candidate);
+            maxObj = std::max(maxObj, fcandidate);
+            if (minObj < fcandidate) {
+                minObj = fcandidate;
+                best = indices;
             }
-            std::cout << '\n' << result << '\n';
+            minObj = std::min(minObj, static_cast<float>(candidate));
+            meanObj += candidate;
         }
+        meanObj /= n;
+
+        // Rest of the stats
+        std::cout << instancePath << ",random," << minObj << ',' << maxObj << ',' << meanObj << ',';
+
+        // Best indices
+        auto const allButLast = best | std::ranges::views::take(best.size() - 1);
+        for (auto const& index : allButLast)
+            std::cout << index << ';';
+        std::cout << best.back() << "\n";
     }
     return 0;
 }
