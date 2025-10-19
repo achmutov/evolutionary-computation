c4b8292002ce4a1942d0288e3dab3f6f7bfbb679
Andrew Chmutov <achmutov@proton.me>
2025-10-19 10:30:36 +0000

    fix & feat: regret greedy nearest neighbor any position
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- include/evolutionary_computation/solver/nearest_neighbor_pos.h
+++ include/evolutionary_computation/solver/nearest_neighbor_pos.h
@@ -1,12 +1,24 @@
 #pragma once
 
 #include <evolutionary_computation/solver/base.h>
+#include <evolutionary_computation/solver/mode.h>
 
 class NearestNeighborPosSolver : public Solver {
 public:
     using Solver::Solver;
 
+    NearestNeighborPosSolver(Mode mode);
+
     virtual std::string name() const override;
 protected:
+    Mode mode;
+
     virtual Indices _solve(int i) override;
+
+    int get_delta(int city, int pos, Indices& indices);
+    std::tuple<int, int> next_greedy(Indices& indices, std::vector<bool>& visited);
+
+    std::tuple<int, int, int> get_regret(int city, Indices& indices);
+    std::tuple<int, int> next_regret(Indices& indices, std::vector<bool>& visited);
+    std::tuple<int, int> next_weighted_regret(Indices& indices, std::vector<bool>& visited);
 };
--- src/solver/nearest_neighbor_pos.cpp
+++ src/solver/nearest_neighbor_pos.cpp
@@ -1,8 +1,14 @@
 #include <evolutionary_computation/solver/nearest_neighbor_pos.h>
-#include <limits>
+
+NearestNeighborPosSolver::NearestNeighborPosSolver(Mode mode = Mode::None) : mode {mode} {}
 
 std::string NearestNeighborPosSolver::name() const {
-    return std::string("nearest_neighbor_pos");
+    auto result = std::string("nearest_neighbor_pos");
+    if (this->mode == Mode::Regret)
+        result += "_regret";
+    else if (this->mode == Mode::WeightedRegret)
+        result += "_weighted_regret";
+    return result;
 }
 
 NearestNeighborPosSolver::Indices NearestNeighborPosSolver::_solve(int i) {
@@ -16,52 +22,115 @@ NearestNeighborPosSolver::Indices NearestNeighborPosSolver::_solve(int i) {
     indices.push_back(i);
     visited[i] = true;
 
-    for (int j = 1; j < half; j++) {
-        // Identify closest node to the current path
-        int closest = -1;
-        int closestVisitedPos = -1;
-        auto minObj = std::numeric_limits<double>::max();
-        for (int iToAdd = 0; iToAdd < n; iToAdd++) {
-            if (visited[iToAdd]) continue;
-            for (size_t pos = 0; pos < indices.size(); pos++) {
-                auto const iInPath = indices[pos];
-                auto const currentObj = this->distances[iInPath][iToAdd] + this->data[iToAdd].cost;
-                if (minObj > currentObj) {
-                    minObj = currentObj;
-                    closest = iToAdd;
-                    closestVisitedPos = pos;
-                }
-            }
-        }
+    auto next = &NearestNeighborPosSolver::next_greedy;
+    if (this->mode == Mode::Regret)
+        next = &NearestNeighborPosSolver::next_regret;
+    else if (this->mode == Mode::WeightedRegret)
+        next = &NearestNeighborPosSolver::next_weighted_regret;
 
-
-        // Decide whether to insert before or after the closest visited node
-        auto prevPos = (closestVisitedPos - 1 + indices.size()) % indices.size();
-        auto nextPos = (closestVisitedPos + 1) % indices.size();
-        auto candidatePos = std::vector<std::pair<int, int>> {
-            {prevPos, closestVisitedPos},
-            {closestVisitedPos, nextPos}
-        };
-        int closestPos = -1;
-        auto minDelta = std::numeric_limits<int>::max();
-        for (auto const [prevPos, pos] : candidatePos) {
-            auto before = indices[prevPos];
-            auto after = indices[pos];
-            auto delta =
-                this->data[closest].cost
-                + this->distances[before][closest]
-                + this->distances[closest][after]
-                - this->distances[before][after];
-
-            if (minDelta > delta) {
-                minDelta = delta;
-                closestPos = pos;
-            }
-        }
-
-        indices.insert(indices.begin() + closestPos, closest);
-        visited[closest] = true;
+    for (int k = 1; k < half; k++) {
+        auto [bestCity, bestPos] = (*this.*next)(indices, visited);
+        indices.insert(indices.begin() + bestPos, bestCity);
+        visited[bestCity] = true;
     }
 
     return indices;
+}
+
+int NearestNeighborPosSolver::get_delta(int city, int pos, Indices& indices) {
+    if (pos == 0) {
+        auto after = indices[pos];
+        return this->data[city].cost + this->distances[city][after];
+    } else if (pos == indices.size()) {
+        auto before = indices[pos - 1];
+        return this->data[city].cost + this->distances[before][city];
+    }
+    auto before = indices[pos - 1];
+    auto after = indices[pos];
+    return this->data[city].cost
+        + this->distances[before][city]
+        + this->distances[city][after]
+        - this->distances[before][after];
+}
+
+std::tuple<int, int> NearestNeighborPosSolver::next_greedy(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    auto minDelta = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+
+        for (size_t pos = 0; pos <= indices.size(); ++pos) {
+            auto delta = this->get_delta(j, pos, indices);
+            if (minDelta > delta) {
+                minDelta = delta;
+                bestCity = j;
+                bestPos = pos;
+            }
+        }
+    }
+
+    return { bestCity, bestPos };
+}
+
+std::tuple<int, int, int> NearestNeighborPosSolver::get_regret(int city, Indices& indices) {
+        int minDelta1 = std::numeric_limits<int>::max();
+        int minDelta2 = std::numeric_limits<int>::max();
+        int bestPos = -1;
+
+        for (size_t pos = 0; pos <= indices.size(); ++pos) {
+            auto delta = this->get_delta(city, pos, indices);
+            if (delta < minDelta1) {
+                minDelta2 = minDelta1;
+                minDelta1 = delta;
+                bestPos = pos;
+            } else if (delta < minDelta2) {
+                minDelta2 = delta;
+            }
+        }
+        auto regret = minDelta2 - minDelta1;
+        return { minDelta1, bestPos, regret };
+}
+
+std::tuple<int, int> NearestNeighborPosSolver::next_regret(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    int maxRegret = -1;
+    int minCostForMaxRegretCity = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+        auto [currentMinDelta, currentBestPos, currentRegret] = this->get_regret(j, indices);
+        if (currentRegret > maxRegret || currentMinDelta < minCostForMaxRegretCity) {
+            maxRegret = currentRegret;
+            bestCity = j;
+            bestPos = currentBestPos;
+            minCostForMaxRegretCity = currentMinDelta;
+        }
+    }
+
+    return { bestCity, bestPos };
+}
+
+std::tuple<int, int> NearestNeighborPosSolver::next_weighted_regret(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    int maxScore = -1;
+    auto minDeltaForMaxRegret = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+        auto [currentMinDelta, currentBestPos, currentRegret] = this->get_regret(j, indices);
+        auto score = currentRegret - currentMinDelta;
+        if (score > maxScore || currentMinDelta < minDeltaForMaxRegret) {
+            maxScore = score;
+            bestCity = j;
+            bestPos = currentBestPos;
+            minDeltaForMaxRegret = currentMinDelta;
+        }
+    }
+
+    return { bestCity, bestPos };
+
 }
