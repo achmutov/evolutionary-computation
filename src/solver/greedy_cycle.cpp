dc37a4d3f53d02014e852af56f19d6894305d168
Andrew Chmutov <achmutov@proton.me>
2025-10-19 10:30:06 +0000

    feat: regret greedy cycle
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- src/solver/greedy_cycle.cpp
+++ src/solver/greedy_cycle.cpp
@@ -1,7 +1,14 @@
 #include <evolutionary_computation/solver/greedy_cycle.h>
 
+GreedyCycle::GreedyCycle(Mode mode = Mode::None) : mode {mode} {}
+
 std::string GreedyCycle::name() const {
-    return std::string("greedy_cycle");
+    auto result = std::string("greedy_cycle");
+    if (this->mode == Mode::Regret)
+        result += "_regret";
+    else if (this->mode == Mode::WeightedRegret)
+        result += "_weighted_regret";
+    return result;
 }
 
 GreedyCycle::Indices GreedyCycle::_solve(int i) {
@@ -15,34 +22,108 @@ GreedyCycle::Indices GreedyCycle::_solve(int i) {
     indices.push_back(i);
     visited[i] = true;
 
+    auto next = &GreedyCycle::next_greedy;
+    if (this->mode == Mode::Regret)
+        next = &GreedyCycle::next_regret;
+    else if (this->mode == Mode::WeightedRegret)
+        next = &GreedyCycle::next_weighted_regret;
+
     for (int k = 1; k < half; k++) {
-        int current = -1;
-        int currentPos = -1;
-        auto minDelta = std::numeric_limits<int>::max();
-
-        for (int j = 0; j < n; ++j) {
-            if (visited[j]) continue;
-
-            for (size_t pos = 1; pos <= indices.size(); ++pos) {
-                auto before = indices[pos - 1];
-                auto after = indices[pos % indices.size()];
-                auto delta =
-                    this->data[j].cost
-                    + this->distances[before][j]
-                    + this->distances[j][after]
-                    - this->distances[before][after];
-
-                if (minDelta > delta) {
-                    minDelta = delta;
-                    current = j;
-                    currentPos = pos;
-                }
-            }
-        }
-
-        indices.insert(indices.begin() + currentPos, current);
-        visited[current] = true;
+        auto [bestCity, bestPos] = (*this.*next)(indices, visited);
+        indices.insert(indices.begin() + bestPos, bestCity);
+        visited[bestCity] = true;
     }
 
     return indices;
+}
+
+int GreedyCycle::get_delta(int city, int pos, Indices& indices) {
+    auto before = indices[pos - 1];
+    auto after = indices[pos % indices.size()];
+    return this->data[city].cost
+        + this->distances[before][city]
+        + this->distances[city][after]
+        - this->distances[before][after];
+}
+
+
+std::tuple<int, int> GreedyCycle::next_greedy(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    auto minDelta = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+
+        for (size_t pos = 1; pos <= indices.size(); ++pos) {
+            auto delta = this->get_delta(j, pos, indices);
+            if (minDelta > delta) {
+                minDelta = delta;
+                bestCity = j;
+                bestPos = pos;
+            }
+        }
+    }
+
+    return { bestCity, bestPos };
+}
+
+std::tuple<int, int, int> GreedyCycle::get_regret(int city, Indices& indices) {
+    int minDelta1 = std::numeric_limits<int>::max();
+    int minDelta2 = std::numeric_limits<int>::max();
+    int bestPos = -1;
+
+    for (size_t pos = 1; pos <= indices.size(); ++pos) {
+        auto delta = this->get_delta(city, pos, indices);
+        if (minDelta1 > delta) {
+            minDelta2 = minDelta1;
+            minDelta1 = delta;
+            bestPos = pos;
+        } else if (minDelta2 > delta) {
+            minDelta2 = delta;
+        }
+    }
+    auto regret = minDelta2 - minDelta1;
+    return { minDelta1, bestPos, regret };
+}
+
+std::tuple<int, int> GreedyCycle::next_regret(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    int maxRegret = -1;
+    int minCostForMaxRegretCity = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+        auto [currentMinDelta, currentBestPos, currentRegret] = this->get_regret(j, indices);
+        if (currentRegret > maxRegret || currentMinDelta < minCostForMaxRegretCity) {
+            maxRegret = currentRegret;
+            bestCity = j;
+            bestPos = currentBestPos;
+            minCostForMaxRegretCity = currentMinDelta;
+        }
+    }
+
+    return { bestCity, bestPos };
+}
+
+std::tuple<int, int> GreedyCycle::next_weighted_regret(Indices& indices, std::vector<bool>& visited) {
+    int bestCity = -1;
+    int bestPos = -1;
+    int maxScore = -1;
+    auto minDeltaForMaxRegret = std::numeric_limits<int>::max();
+
+    for (int j = 0; j < visited.size(); ++j) {
+        if (visited[j]) continue;
+        auto [currentMinDelta, currentBestPos, currentRegret] = this->get_regret(j, indices);
+        auto score = currentRegret - currentMinDelta;
+        if (score > maxScore || currentMinDelta < minDeltaForMaxRegret) {
+            maxScore = score;
+            bestCity = j;
+            bestPos = currentBestPos;
+            minDeltaForMaxRegret = currentMinDelta;
+        }
+    }
+
+    return { bestCity, bestPos };
 }
